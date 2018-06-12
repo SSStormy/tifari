fae7c4d45484895a94dbdc4591bb8b56186c2949
Justas Dabrila <justasdabrila@gmail.com>
2018-06-12 18:22:14 +0000

    frontend: added about

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -42,7 +42,10 @@ import Select from '@material-ui/core/Select';
 import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 import ListItemAvatar from '@material-ui/core/ListItemAvatar';
 import Avatar from '@material-ui/core/Avatar';
+import SvgIcon from '@material-ui/core/SvgIcon';
 import ChipInput from 'material-ui-chip-input'
+
+const VERSION = "1.0.0";
 
 const allOrderings = [
     {
@@ -298,6 +301,11 @@ class StateMutator {
         return this;
     }
 
+    setDialogAboutState(state) {
+        this.newState.dialogAbout = state;
+        return this;
+    }
+
     setDialogImageRowsState(state) {
         this.newState.dialogImageRowsOpen = state;
         return this;
@@ -384,6 +392,7 @@ class App extends Component {
             isDrawerOpen: false,
             displayTagList: false,
             dialogImageRowsOpen: false,
+            dialogAbout: false,
             dialogBackendUrl: false,
             tagQueueSize: 0,
             searchTagNames: [],
@@ -634,6 +643,10 @@ class App extends Component {
         });
     }
 
+    setDialogAboutState(state) { 
+        this.mutateState(mut => mut.setDialogAboutState(state));
+    }
+
     swapBackendUrl() {
         this.state.api.setEndpoint(this.state.backendUrlBuffer);
     }
@@ -800,6 +813,11 @@ class App extends Component {
 
                         <List component="div" dense>
                             <ListItem>
+
+                                <ListItemIcon>
+                                    <Icon>sort</Icon>
+                                </ListItemIcon>
+
                                 <Select style={{width: "100%"}}
                                     value={this.state.tagOrdering.id}
                                     onChange={(e) => this.mutateState(mut => mut.setTagOrdering(e.target.value).orderTags())}
@@ -899,7 +917,17 @@ class App extends Component {
                         <ListItemText primary="Set backend URL"/>
                     </ListItem>
 
-                    {/* TODO: about that links to github, shows version etc*/}
+                    <Divider/>
+
+                    <ListItem button
+                        onClick={() => this.setDialogAboutState(true)}
+                        >
+                        <ListItemIcon>
+                            <Icon>star</Icon>
+                        </ListItemIcon>
+
+                        <ListItemText primary="About"/>
+                    </ListItem>
 
                 </Drawer>
 
@@ -908,6 +936,44 @@ class App extends Component {
                         {imageList}
                     </Grid>
                 </div>
+
+                <Dialog
+                    open={this.state.dialogAbout}
+                    onClose={() => this.setDialogAboutState(false)}
+                    >
+
+                    <DialogTitle>{`Tifari web frontend v${VERSION}`}</DialogTitle>
+
+                    <DialogContent>
+                        <DialogContentText>
+                            Built using <a href="https://github.com/facebook/react/">react</a>, <a href="https://github.com/mui-org/material-ui">material-ui</a>, <a href="https://github.com/TeamWertarbyte/material-ui-chip-input">material-ui-chip-input</a>
+                        </DialogContentText>
+
+                        <List dense>
+                            <ListItem>
+
+                                <ListItemIcon>
+                                    <SvgIcon>
+                                        <path d="M12.007 0C6.12 0 1.1 4.27.157 10.08c-.944 5.813 2.468 11.45 8.054 13.312.19.064.397.033.555-.084.16-.117.25-.304.244-.5v-2.042c-3.33.735-4.037-1.56-4.037-1.56-.22-.726-.694-1.35-1.334-1.756-1.096-.75.074-.735.074-.735.773.103 1.454.557 1.846 1.23.694 1.21 2.23 1.638 3.45.96.056-.61.327-1.178.766-1.605-2.67-.3-5.462-1.335-5.462-6.002-.02-1.193.42-2.35 1.23-3.226-.327-1.015-.27-2.116.166-3.09 0 0 1.006-.33 3.3 1.23 1.966-.538 4.04-.538 6.003 0 2.295-1.5 3.3-1.23 3.3-1.23.445 1.006.49 2.144.12 3.18.81.877 1.25 2.033 1.23 3.226 0 4.607-2.805 5.627-5.476 5.927.578.583.88 1.386.825 2.206v3.29c-.005.2.092.393.26.507.164.115.377.14.565.063 5.568-1.88 8.956-7.514 8.007-13.313C22.892 4.267 17.884.007 12.008 0z" />
+                                    </SvgIcon>
+                                </ListItemIcon>
+
+                                <ListItemText>
+                                    <a href="https://github.com/SSStormy/tifari">GitHub</a>
+                                </ListItemText>
+
+                            </ListItem>
+                        </List>
+
+                    </DialogContent>
+
+                    <DialogActions>
+                        <Button onClick={() => this.setDialogAboutState(false)} color="primary">
+                            Close 
+                        </Button>
+                    </DialogActions>
+
+                </Dialog>
 
                 <Dialog
                     open={this.state.dialogBackendUrl}
