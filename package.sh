e4532ce8e31a49819889796e88a2e01d5efa0047
Justas Dabrila <justasdabrila@gmail.com>
2018-07-04 23:27:16 +0300

    added a package script, use rusqlite's bundled feature

---
+++ package.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "===> Building server"
+cd prod
+cargo build --release
+cd ..
+
+echo "===> Building frontend"
+cd frontend
+npm run build
+cd ..
+
+echo "===> Packaging"
+rm -rf release
+mkdir release
+cd release
+cp ../prod/target/release/prod.exe tifari.exe
+cp -r ../frontend/build static
+cd ..
+
+echo "===> Done!"
